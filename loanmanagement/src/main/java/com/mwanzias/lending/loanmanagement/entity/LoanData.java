75de6acb18a6cf9f57e687aab2ecfe1988a5a24b
mwanzias <mwanzias@gmail.com>
2025-03-12 08:55:54 +0300

    Add loan data Entity for managing the loans

--- loanmanagement/src/main/java/com/mwanzias/lending/loanmanagement/entity/LoanData.java
+++ loanmanagement/src/main/java/com/mwanzias/lending/loanmanagement/entity/LoanData.java
@@ -1,8 +1,11 @@
 package com.mwanzias.lending.loanmanagement.entity;
 
+import java.util.Date;
+
 public class LoanData {
     private long loanId;
     private String customerNationalId;
     private long loanAmount;
     private long loanProductId;
+    private Date loanRequestDate;
 }
