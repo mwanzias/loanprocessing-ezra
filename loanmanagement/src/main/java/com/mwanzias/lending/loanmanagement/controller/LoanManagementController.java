10169e615c494a76b040af63e653242a1f3f7d8f
mwanzias <mwanzias@gmail.com>
2025-03-12 09:01:18 +0300

    Add validation to the loan entity in loanmanagement

--- loanmanagement/src/main/java/com/mwanzias/lending/loanmanagement/controller/LoanManagementController.java
+++ loanmanagement/src/main/java/com/mwanzias/lending/loanmanagement/controller/LoanManagementController.java
@@ -1,4 +1,8 @@
 package com.mwanzias.lending.loanmanagement.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class LoanManagementController {
 }
--- loanmanagement/src/main/java/com/mwanzias/lending/loanmanagement/entity/LoanData.java
+++ loanmanagement/src/main/java/com/mwanzias/lending/loanmanagement/entity/LoanData.java
@@ -1,9 +1,19 @@
 package com.mwanzias.lending.loanmanagement.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Date;
 
+@Entity
 public class LoanData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // This would be changed in the future to have it with some specific structure, have a service function that generated IDs
     private long loanId;
+    @NotBlank(message = "The national Id is required ")
+    @Size(min = 5, max=9, message = "The national ID does not seem to be valid")
     private String customerNationalId;
     private long loanAmount;
     private long loanProductId;
