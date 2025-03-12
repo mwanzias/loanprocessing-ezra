db54f062e5bd2a6835d1dd0925a24730bd698fd8
mwanzias <mwanzias@gmail.com>
2025-03-12 09:10:25 +0300

    Add getters and setters for the loan entity under loan management service

--- loanmanagement/src/main/java/com/mwanzias/lending/loanmanagement/entity/LoanData.java
+++ loanmanagement/src/main/java/com/mwanzias/lending/loanmanagement/entity/LoanData.java
@@ -1,10 +1,14 @@
 package com.mwanzias.lending.loanmanagement.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.core.Local;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
@@ -12,10 +16,68 @@ public class LoanData {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO) // This would be changed in the future to have it with some specific structure, have a service function that generated IDs
     private long loanId;
+    @NotBlank(message = "The loan cannot be blank")
+    @Min(value = 50, message="The loan cannot be less than 50")
+    @Max(value = 100000, message = "The loan amount cannot be greater than 100000")
+    private long loanAmount;
+    @NotBlank(message = "The loan must have a correlating loan product ID")
+    private long loanProductId;
+    private LocalDateTime loanRequestDate;
+    private LocalDateTime loanDueDate;
+
+    public LoanData() {
+    }
+
     @NotBlank(message = "The national Id is required ")
     @Size(min = 5, max=9, message = "The national ID does not seem to be valid")
     private String customerNationalId;
-    private long loanAmount;
-    private long loanProductId;
-    private Date loanRequestDate;
+
+    public LocalDateTime getLoanDueDate() {
+        return loanDueDate;
+    }
+
+    public void setLoanDueDate(LocalDateTime loanDueDate) {
+        this.loanDueDate = loanDueDate;
+    }
+
+    public LocalDateTime getLoanRequestDate() {
+        return loanRequestDate;
+    }
+
+    public void setLoanRequestDate(LocalDateTime loanRequestDate) {
+        this.loanRequestDate = loanRequestDate;
+    }
+
+    public long getLoanProductId() {
+        return loanProductId;
+    }
+
+    public void setLoanProductId(long loanProductId) {
+        this.loanProductId = loanProductId;
+    }
+
+    public long getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(long loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public String getCustomerNationalId() {
+        return customerNationalId;
+    }
+
+    public void setCustomerNationalId(String customerNationalId) {
+        this.customerNationalId = customerNationalId;
+    }
+
+    public long getLoanId() {
+        return loanId;
+    }
+
+    @PrePersist
+    protected void onCreate() { // give the loanreuestDate a date of today
+        this.loanRequestDate = LocalDateTime.now();
+    }
 }
